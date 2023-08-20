5227867d8759aefbbcfafb3beb137e300331881a
fahmi <fahmiabdulaziz44@gmail.com>
2023-08-20 13:19:53 +0700

    Initial Commit

---
+++ Pinch App/Pinch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_AppApp.swift
+//  Pinch App
+//
+//  Created by Fahmi Aziz on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
